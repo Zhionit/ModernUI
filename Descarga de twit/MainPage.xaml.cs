2b22cd28daf9c028792f799b5c0f224e567fe121
Marlon Ramirez (Zhionit) <zhionit@live.com>
2014-01-12 16:13:10 -0500

    Ejemplo de como descargargar un twit de manera asincrona

---
+++ Descarga de twit/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Descarga_de_twit
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Se invoca cuando esta página se va a mostrar en un objeto Frame.
+        /// </summary>
+        /// <param name="e">Datos de evento que describen cómo se llegó a esta página. La propiedad Parameter
+        /// se usa normalmente para configurar la página.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=twitterapi&count=2
+            // https://api.twitter.com/1/statuses/user_timeline.json?screen_name=warnov&count=1
+            
+            string url = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=twitterapi&count=2";
+            
+            var http = new HttpClient();
+
+            // Genera excepción "El código de usuario no controló System.Net.Http.HttpRequestException -  Message=Response status code does not indicate success: 410 (Gone)."
+            var jsonText = await http.GetStringAsync(url);
+            var jasonValue = JsonArray.Parse(jsonText)[0];
+            textBlock1.Text = jasonValue.GetObject().GetNamedString("text");
+        }
+    }
+}
