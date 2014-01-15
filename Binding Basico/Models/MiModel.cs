f8fc502e06ac832a1eaba27d708a8304df147739
Marlon Ramirez (Zhionit) <zhionit@live.com>
2014-01-14 21:55:51 -0500

    Ejemplo de como usar Binding Básico

---
+++ Binding Basico/Models/MiModel.cs
@@ -0,0 +1,55 @@
+﻿using App4.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4.Models
+{
+    public class MiModel: BindableBase
+    {
+        // Los comentarios son la lógica básica para notificarle a la interfaz de algún Cambio de implementacion de INotiFyPropertyChanged
+        // Que es cambiada por BindableBase Que se optiene al crear una BasicPage
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //public int MyProperty { get; set; } // Propiedades autoimplementada
+
+        private String _texto1; //Variable de backStore, usado para el binding
+        public String Texto1
+        {
+            get { return _texto1; }
+            set
+            {
+                SetProperty(ref _texto1, value);
+                //_texto1 = value;
+                //RaisePropertyChanged();
+            }
+        }
+
+        private String _texto2;
+
+        public String Texto2
+        {
+            get { return _texto2; }
+            set
+            {
+                SetProperty(ref _texto2, value);
+                //_texto2 = value;
+                //RaisePropertyChanged();
+            }
+        }
+
+
+        //private void RaisePropertyChanged([CallerMemberName] String propertyName = "")
+        //{
+        //    if (PropertyChanged != null)
+        //    {
+        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
+
+    }
+}
---
+++ Binding Basico/Views/BindingPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using App4.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App4.Views
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class BindingPage : Page
+    {
+
+        MiModel myModel = new MiModel();
+
+        public BindingPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Se invoca cuando esta página se va a mostrar en un objeto Frame.
+        /// </summary>
+        /// <param name="e">Datos de evento que describen cómo se llegó a esta página. La propiedad Parameter
+        /// se usa normalmente para configurar la página.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.DataContext = myModel;
+            myModel.Texto1 = "Hola Mundo";
+            myModel.Texto2 = "Hello Word!";
+        }
+
+        
+    }
+}
