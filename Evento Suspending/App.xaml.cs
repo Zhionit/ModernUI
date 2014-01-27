7df40713c9ba7b9c4862cfef5504b566b8ed4efd
Marlon Ramirez (Zhionit) <zhionit@live.com>
2014-01-27 18:46:40 -0500

    Ejmplo de como aprovechar cuando nuestra aplicación entra en modo Suspended Para guardar lo que necesitamos.

---
+++ Evento Suspending/App.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla Aplicación vacía está documentada en http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace Evento_Suspending
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        protected async override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+
+            #if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+            #endif
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+
+                Evento_Suspending.Common.SuspensionManager.RegisterFrame(rootFrame, "appFrame");
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    await Evento_Suspending.Common.SuspensionManager.RestoreAsync();
+                }
+
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                if (!rootFrame.Navigate(typeof(PaginaInicio), e.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+            Window.Current.Activate();
+        }
+
+        // Se invoca cuando se produce un error en la navegación a una página determinada
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private async void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+
+            // Guarda el estado de navegación del Frame y luego da a la Page la oportunidad de guardar su contenido
+            await Evento_Suspending.Common.SuspensionManager.SaveAsync();
+
+            deferral.Complete();
+        }
+    }
+}
---
+++ Evento Suspending/PaginaInicio.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Evento_Suspending.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página básica está documentada en http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace Evento_Suspending
+{
+    public sealed partial class PaginaInicio : Page
+    {
+
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public PaginaInicio()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            // Comprueba si existe el diccionario PageState y tiene una clave llamada greetingOutputText
+            if (e.PageState != null && e.PageState.ContainsKey("greetingOutputText"))
+            {
+                // Si la encuentra, la úsamos para restaurar el texto greetingOutput
+                greetingOutput.Text = e.PageState["greetingOutputText"].ToString();
+            }
+
+            // Para que los datos de usuario persistan durante varias sesiones, lo almacenamos en el contenedor de datos de la aplicación RoamingSettings
+            Windows.Storage.ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+
+            if (roamingSettings.Values.ContainsKey("userName"))
+            {
+                nameInput.Text = roamingSettings.Values["userName"].ToString();
+            }
+        }
+
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+            e.PageState["greetingOutputText"] = greetingOutput.Text;
+            e.PageState["nameImput"] = nameInput.Text;
+        }
+
+        #region Registro de NavigationHelper
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void nameInput1_Click(object sender, RoutedEventArgs e)
+        {
+            greetingOutput.Text = "Hello, " + nameInput.Text + "!";
+        }
+
+        private void NameInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Windows.Storage.ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+            roamingSettings.Values["userName"] = nameInput.Text;
+        }
+    }
+}
